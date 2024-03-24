c0653f88dc78b4157dd02505880a6f5ca8dcc893
Soare <soarenexus@gmail.com>
2024-03-24 12:21:44 +0100

    ok

--- .netlify/functions-internal/ssr/chunks/carta_Dg3L6oGo.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/carta_CzwDK3MZ.mjs').then(n => n.c);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_CPM5j0G5.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_C9JK2gA9.mjs');
-
-export { page };
